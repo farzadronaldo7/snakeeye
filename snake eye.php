0619f803b5113308072a1fad51a66db650acc589
Farzad Ronaldo <32195032+farzadronaldo7@users.noreply.github.com>
2017-11-03 10:26:48 +0000

    Update snake eye.php

--- snake eye.php
+++ snake eye.php
@@ -6,13 +6,18 @@
 <?php>
 
 $t = rand(1,6);
+$r = rand(1,6);
 echo $t;
 echo "<br>";
-	
-	if ($t == 1) {
+echo "$r;
+echo "<br>";
+
+
+	if ($t == 1 and $r == 1) {
 	echo "snake eyes";
 	}
-	elseif ($t == 6) {
+	
+	elseif ($t == 6 or $r == 6) {
 		echo "winner";
 	}
 	
